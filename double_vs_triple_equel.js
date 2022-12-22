f2fb479590a118912dad07ba7bd47c0a9b10d189
Md Iffat Bari <88897305+mdiffatbari@users.noreply.github.com>
2022-12-23 00:11:25 +0600

    equel check

---
+++ double_vs_triple_equel.js
@@ -0,0 +1,9 @@
+const num1 = 4;
+const num2 = "4";
+
+if (num1 === num2) {
+    console.log("Condition is true");
+}
+else {
+    console.log("Condition is false");
+}
--- null_vs_undefined.js
+++ null_vs_undefined.js
@@ -8,7 +8,7 @@ console.log(result); */
 let num;
 console.log(num);
 
-const age = [20, 55, 4, 5];
+const age = [20, 55, 4, 5git];
 console.log(age[8]);
 
 const details = { name: 'iffat', Age: 22, phone_no: 0123456 };
