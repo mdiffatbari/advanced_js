ce0a091e45553b23c3d658c2964c759251394d3c
Md Iffat Bari <88897305+mdiffatbari@users.noreply.github.com>
2022-12-22 23:57:49 +0600

    null vs undefined

---
+++ null_vs_undefined.js
@@ -0,0 +1,15 @@
+function add(num1, num2) {
+    const sum = num1 + num2;
+}
+
+const result = add(5, 100);
+console.log(result); */
+
+let num;
+console.log(num);
+
+const age = [20, 55, 4, 5];
+console.log(age[8]);
+
+const details = { name: 'iffat', Age: 22, phone_no: 0123456 };
+console.log(details.school);
