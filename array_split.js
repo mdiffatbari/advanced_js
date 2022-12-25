76720085f89d8cc4dacba6f629083b32df7d8f08
Md Iffat Bari <88897305+mdiffatbari@users.noreply.github.com>
2022-12-25 20:33:35 +0600

    array split done

---
+++ array_split.js
@@ -0,0 +1,17 @@
+const nums = [20, 11, 45, 77, 87];
+
+
+//const numsPart = nums.slice(1, 3);
+// console.log(numsPart);
+// console.log(nums);
+
+
+//const removeNumber = nums.splice(1, 3, 55);
+// console.log(removeNumber);
+// console.log(nums);
+
+
+const togetherNums = nums.join("....");
+console.log(togetherNums);
+
+
